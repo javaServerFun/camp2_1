1471d60e03d82273d2d992e2d75988a506129a95
Jarek Ratajski <jratajski@gmail.com>
2018-09-29 14:01:04 +0200

    lesson4_end

--- src/main/java/pl/survival/camp2/servlets/Calculator.java
+++ src/main/java/pl/survival/camp2/servlets/Calculator.java
@@ -11,12 +11,20 @@ import java.io.PrintWriter;
 @WebServlet(name = "Calculator", urlPatterns = "/calc")
 public class Calculator extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
         PrintWriter writer = response.getWriter();
         String arga = request.getParameter("a");
         String argb = request.getParameter("b");
-        int a = Integer.parseInt(arga);
-        int b = Integer.parseInt(argb);
-        writer.println("wynik=" + (a+b));
+        try {
+            int a = Integer.parseInt(arga);
+            int b = Integer.parseInt(argb);
+            writer.println("wynik=" + (a + b));
+            writer.println("<br/><a href='/index.html'>powrót</a>");
+        } catch (NumberFormatException nfe) {
+            writer.println("Błędne dane");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
 
     }
 }
